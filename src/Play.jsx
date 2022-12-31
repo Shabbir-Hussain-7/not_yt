f262540e4976293d89851c1911b5ca2b9f366bc4
Shabbir-Hussain-7 <2019n00032@gmail.com>
2022-12-31 15:37:47 +0500

    deleted unnecessary files

--- src/Play.jsx
+++ src/Play.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 import Box from '@mui/material/Box';
 import { useParams } from "react-router-dom";
-import fetchData from "./fetchData";
-import { useEffect } from "react";
 import ReactPlayer from "react-player";
 
 const Play = () => {
     const { videoId } = useParams();
-    const [video, setVideo] = React.useState({});
-    useEffect(() => {
-        const data = fetchData(videoId);
-        data.then((result) => {
-            setVideo(result.items[0]);
-        });
-    }, [videoId]);
     return (
         <Box style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "50vh", width: "40vw", margin: "auto"}}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${videoId}`} width="100%" height="100%" controls/>
